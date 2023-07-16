4c2087c94c65bb13c2e0a1f63521843f2cb0e00d
Zhuk20 <ghuk200067@gmail.com>
2023-07-16 13:30:08 +1200

    Первый коммит

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+        try {
+            Validator.check("login", "Pass", "Pass");
+            System.out.println("Вы успешно зарегистрировались");
+        } catch (WrongLoginException | WrongPasswordException e){
+            throw new RuntimeException(e);
+        }
     }
 }
